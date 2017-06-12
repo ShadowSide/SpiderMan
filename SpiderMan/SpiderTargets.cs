54e6acba87661c193b845ee227c7603c12d5e2e5
ShadowSide <g77766613@yandex.ru>
2017-06-12 20:55:54 +0300

    garbage
    Добавьте файлы проекта.
    
    Добавьте GITIGNORE- и GITATTRIBUTES-файлы.

---
+++ SpiderMan/SpiderTargets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiderMan
+{
+    public class WebSpiderTargets
+    {
+        private Uri[] _baseUri;
+
+        public WebSpiderTargets(Uri[] baseUri)
+        {
+            _baseUri = baseUri
+        }
+
+        public Uri[] BaseUri {get { return _baseUri; } }
+    }
+}
---
+++ SpiderMan/WebSpider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+
+namespace SpiderMan
+{
+    public class WebSpider: IDisposable
+    {
+        private WebSpiderTargets _targets;
+        private WebClient _client;
+
+        public WebSpider(WebSpiderTargets targets)
+        {
+            _targets = targets;
+            _client = new WebClient();
+        }
+
+        public void Crowle(Uri uri)
+        {
+            _client;
+            HttpWebRequest g;
+            g.
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+    }
+}
